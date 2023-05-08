d9594c0ede586e365ef90091d9dc1bb62abd72e2
White_Panther <99096313+White-Panther420@users.noreply.github.com>
2023-05-08 14:40:31 -0700

    Game is now fully functional for Vs Player Option
    I got the scores to be reset correctly. Now I need to work on making the AI play on it's own.

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 const GUI = document.querySelector(".GUI")
+const player1GUI = document.querySelector(".LeftGUI")
 const AIGUI = document.querySelector(".RightGUI")
 const Player2GUI = document.querySelector(".RightGUIPlayer2")
 const StartScreenContent = document.querySelector(".StartScreenContent")
@@ -8,23 +9,30 @@ const player1Score = document.querySelector(".LeftScore");
 const player2Score = document.querySelector(".Player2Score");
 const AIScore = document.querySelector(".RightScore")
 const restartBtn = document.querySelector(".Restart")
+const QuitBtn = document.querySelector(".QuitBtn")
 let AISelection = false; //Used to identify the correct right-side GUI
-let win = false;  //Used to stop user from clicking on squares
+let winRound = false;  //Used to stop user from clicking on squares
+let winGame = false;
+let quitGame = false;
 /******************************Start Screen******************************/
 const Player2Btn = document.querySelector(".vsPlayer")
 Player2Btn.addEventListener("click", ()=>{
+    restartBtn.style.display = "flex"
     AISelection = false;
     StartScreenContent.style.display = "none"
     gameContent.style.display = "flex"
     Player2GUI.style.display = "block"
+    displayGame.displayBoard();
 })
 
 const AIBtn = document.querySelector(".VsAI")
 AIBtn.addEventListener("click", ()=>{
+    restartBtn.style.display = "flex"
     AISelection = true;
     StartScreenContent.style.display = "none"
     gameContent.style.display = "flex"
     AIGUI.style.display = "block"
+    displayGame.displayBoard();
 })
 /******************************Start Screen******************************/
 
@@ -59,7 +67,10 @@ const Player = (name) => {
     const updateScore = () =>{
         return ++score;
     }
-    return{name, token, updateScore}
+    const clearScore = () =>{
+        return score = 0;
+    }
+    return{name, token, updateScore, clearScore}
 };
 
 const gameController = (() => {
@@ -73,7 +84,8 @@ const gameController = (() => {
     }
     const players = [Player("Player 1"), Player(playerName)];
     let currentTurn = 0;
-    let movesCount = 0;
+    let movesCount = 1;
+
     let activeTurn = (currentTurn) =>{
         if(currentTurn === 0)
             return 1; //Player 1 turn
@@ -81,13 +93,15 @@ const gameController = (() => {
             return 0; //Player 2 / AI turn
     };
 
-    const playRound = (square) => {
+    const playTurn = (square) => {
+        quitGame = false
+        winGame = false
         let squarePosition = square.getAttribute("data-state")
         let row = Math.floor(squarePosition/3)+1;
         let col = (squarePosition%3) + 1;
         let token = players[currentTurn].token();
         
-        //Add class to square based on token
+        //Add class to square based on token to modify token appearence 
         if(token === "X"){
             square.classList.add("Player1X")
         }
@@ -97,23 +111,27 @@ const gameController = (() => {
         else{
             square.classList.add("Player2O")
         }
+
+        //Update game board
         square.textContent = token
-        board[col-1][row-1] = token //Update game board
-        
+        board[col-1][row-1] = token 
+        console.log("NUMBER OF MOVES: " + movesCount)
         if(wonRound(board, row, col, token)){
-            let currPlayerScore = players[currentTurn].updateScore()
+            let currPlayerScore = players[currentTurn].updateScore()  //Store the winner's score
             displayGame.updateScoreGUI(token, currPlayerScore)
-            if(currPlayerScore === 5){
-                //End game
+            displayGame.printWinMsg(token, currPlayerScore)
+            winRound = true
+            movesCount = 1
+            if(winGame){
+                displayGame.removeRestartOption()
             }
-            else
-                displayGame.printWinMsg(token)
-            win = true
+            return;
         }
-        // if(movesCount === 9){
-        //     console.log("DRAW!")
-        //     return;
-        // }
+        if(movesCount === 9){
+            displayGame.printDrawMSG()
+            movesCount = 1
+            return;
+        }
         currentTurn = activeTurn(currentTurn);  //Switch turn
         movesCount++;
     }
@@ -151,6 +169,7 @@ const gameController = (() => {
             }
         }
 
+        //Checking for reverse diagonal win
         if((row-1) + (col-1) === 2){
             for(let i=0; i<3; i++){
                 if(board[i][(3-1)-i] !== token){
@@ -162,13 +181,24 @@ const gameController = (() => {
             }
         }
     }
-    return{playRound};
+    
+    const resetScore = ()=>{  //Allows quitBtn to also reset score on backend. Not just GUI
+        players[0].clearScore()
+        players[1].clearScore();
+    }
+
+    return{playTurn, resetScore};
 })();
 
 const displayGame = (() => {
     restartBtn.addEventListener("click", () =>{
         restartGame();
     })
+    QuitBtn.addEventListener("click", ()=>{
+        gameController.resetScore();
+        endGame();
+    })
+
     const displayBoard = () =>{
         for(let i=0; i<9; i++){
             let square = document.createElement("div");
@@ -178,8 +208,12 @@ const displayGame = (() => {
             gameBoardDiv.appendChild(square);
             square.addEventListener("click", func)
             function func(){
-                if(win || (win && square.textContent !== "")){
+                if(winRound || (winRound && square.textContent !== "")){ 
                     //Prevent user from clicking on any square once game is over
+                    square.removeEventListener("click", func)
+                }
+                else if(winGame)
+                {
                     square.removeEventListener("click", func)
                 }
                 else if(square.textContent !== ""){
@@ -188,7 +222,7 @@ const displayGame = (() => {
                     console.log("ILLEGAL MOVE");
                 }
                 else{
-                    gameController.playRound(square)
+                    gameController.playTurn(square)
                 }
             }
         }
@@ -205,9 +239,10 @@ const displayGame = (() => {
         }
     }
 
-    const printWinMsg = (token) =>{
+    const printWinMsg = (token, score) =>{
         let playerName;
         const winMsg = document.querySelector(".win_p");
+        winMsg.classList.add("winMsg")
         if(token === 'O' && !AISelection){
             playerName = "Player 2";
             GUI.insertBefore(winMsg, Player2GUI)
@@ -223,20 +258,62 @@ const displayGame = (() => {
             else
                 GUI.insertBefore(winMsg, Player2GUI)
         }
-        winMsg.textContent = `${playerName} has won the round!`
-        winMsg.classList.add("winMsg")
+        if(score === 2){
+            winMsg.textContent = `${playerName} has won the game!!!!`
+            winGame = true
+        }
+        else{
+            winMsg.textContent = `${playerName} has won the round!`
+        }
+    }
+
+    const printDrawMSG = () =>{
+        const drawMsg = document.querySelector(".draw_p")
+        drawMsg.classList.add("drawMsg")
+        drawMsg.textContent = "This game is a draw!"
+        player1GUI.parentNode.insertBefore(drawMsg, player1GUI.nextSibling);
+    }
+
+    const endGame = ()=>{
+        quitGame = true;
+        gameContent.style.display = "none"
+        const winMsgElement = document.querySelector(".win_p")
+        const drawMsgElement = document.querySelector(".draw_p")
+        winMsgElement.classList.remove("winMsg")
+        drawMsgElement.classList.remove("drawMsg")
+        gameBoard.createNewBoard();
+        gameBoardDiv.textContent = ""
+        player1Score.textContent = "0"
+        if(AISelection){
+            AIGUI.style.display = "none"
+            AIScore.textContent = "0"
+        }
+        else{
+            Player2GUI.style.display = "none"
+            player2Score.textContent = "0"
+        }
+        StartScreenContent.style.display = "flex"
+        winGame = false;
+        winRound = false;
     }
 
     const restartGame = () =>{
         gameBoardDiv.textContent = "";
         const winMsgElement = document.querySelector(".win_p")
+        const drawMsgElement = document.querySelector(".draw_p")
         winMsgElement.classList.remove("winMsg")
-        win = false;
+        drawMsgElement.classList.remove("drawMsg")
+        winRound = false;
         gameBoard.createNewBoard();
         displayBoard();
+        console.log("HIIIII")
     }
-    return{displayBoard, updateScoreGUI, printWinMsg}
+
+    const removeRestartOption = () =>{
+        restartBtn.style.display = "none"
+    }
+    return{displayBoard, updateScoreGUI, printWinMsg, printDrawMSG, removeRestartOption}
 })();
 
-displayGame.displayBoard();
+
 
